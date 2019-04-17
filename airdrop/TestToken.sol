41c417fa8cc8203a3e8eed355c98362f29002e57
swsong <songaal@gmail.com>
2019-04-18 07:07:40 +0900

    airdrop

---
+++ airdrop/TestToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.24;
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+
+//테스트 RRC20 토큰
+contract TestToken is MintableToken {
+    constructor() public {
+
+    }
+}
